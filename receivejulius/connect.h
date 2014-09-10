93f475e09e64fd84a27cb5f5502b1b3e352d9123
leaves160hkp <hata9@tech-mcc.ml>
2014-09-10 21:21:03 +0900

    Cannot solve the error. Sorry...(-_-)

---
+++ receivejulius/connect.h
@@ -0,0 +1,17 @@
+#ifndef CONNECT_H
+#define CONNECT_H
+#include <boost/asio.hpp>
+#include <iostream>
+
+
+using namespace std;
+namespace asio=boost::asio;
+namespace ip=asio::ip;
+
+class tcpConnect{
+  public:
+    tcpConnect();
+    //readline();
+};
+
+#endif
