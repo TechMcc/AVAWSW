cfcb4b80e990084bc08378357c14d54ff126f395
sh4869 <nobuk4869@gmail.com>
2014-09-30 13:04:06 +0900

    remove unuse files

--- receivejulius/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "connect.h"
-
-int main(){
-  tcpConnect hoge;
-  return 0;
-}
--- rubyJulius/speek.sh
+++ rubyJulius/speek.sh
@@ -1,3 +1,3 @@
-open_jtalk -m /usr/local/share/hts_voice/mei_happy.htsvoice -x /usr/local/dic -ow message.wav speak.txt
+open_jtalk -m /usr/local/share/hts_voice/mei_normal.htsvoice -x /usr/local/dic -ow message.wav speak.txt
 mplayer message.wav
 rm message.wav speak.txt
