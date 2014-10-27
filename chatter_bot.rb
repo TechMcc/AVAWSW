b11a4abd5b0d7054fbba2ef8b0816d084bd1d9bb
sh4869 <nobuk4869@gmail.com>
2014-10-28 04:23:20 +0900

    create julius server program

--- chatter_bot.rb
+++ chatter_bot.rb
@@ -1,13 +1,15 @@
 #ChatterBot Class
 
 class Chatter
-  def self.create_reply(sentence) 
-  end
-  
   def self.pattern_match(sentence)
   end
   
   def self.parse_sentence(sentence)
+    
+  end
+  
+  def self.create_reply(sentence) 
+  
   end
 end
 
--- julius_receive.rb
+++ julius_receive.rb
@@ -1,21 +1,53 @@
 require 'socket'
 require 'thread'
+require 'rexml/document'
 
 class Julius  
   def initialize
-  	@julius = TCPSocket.new('localhost',10500)
-  end
-  
-  def self.clear_xmlstr(line)
-    line['<s>'] = ""
-	line['</s>'] = ""
-    return line
+	@receive_mode = 0
   end
 
-  def self.receve_data
-  	while line = @julius.gets
-	  line = clear_xmlstr(line)
-	  if line.include?("<RECOGOUT>")
-		
+  def self.get_word(str)
+	sentence = ""
+	str['<s>'] = ""
+	str['</s>'] = ""
+	xml = REXML::Document.new(str)
+	xml.elements.each('RECOGOUT/SHYPO/WHYPO') do |element|
+	  hash = element.attributes
+	  if hash["WORD"] != nil
+		sentence += hash["WORD"]
+	  end
+	end
+	return sentence 
   end
+
+  def self.receive_data
+	@julius = TCPSocket.new('localhost',10500)
+	receive_mode = 0
+	words = ""
+	while line = @julius.gets 
+	  if receive_mode == 0 #Nomal mode
+		if line.include?("<RECOGOUT>")
+		  receive_mode = 1 
+		  words += line
+		  puts "RECEIVE String!!"
+		end  
+	  elsif receive_mode == 1 #Get Word mode 
+		if line.include?("<\/RECOGOUT>")
+		  #返答パターン作成と文字列の初期化、ノーマルモードへ戻す。
+		  words += line
+		  sentence = get_word(words) 
+		  puts sentence  
+		  words = ""
+		  receive_mode = 0
+		else
+		  words += line
+		end    
+	  end		
+	end
+  end
+
 end
+
+julius = Julius.new
+Julius.receive_data
