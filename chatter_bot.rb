320346e2f72101906f85df5385a00b5cf6891287
sh4869 <nobuk4869@gmail.com>
2014-10-28 15:32:26 +0900

    create  chatter and pattern csv file

--- chatter_bot.rb
+++ chatter_bot.rb
@@ -1,15 +1,22 @@
+require 'csv'
+
 #ChatterBot Class
 
 class Chatter
-  def self.pattern_match(sentence)
+  def pattern_match(sentence)
+    CSV.foreach("pattern.csv") do |pattern|
+      if sentence.include?(pattern[0])
+	    return pattern[1]
+	  end
+	end	  
   end
   
-  def self.parse_sentence(sentence)
-    
-  end
-  
-  def self.create_reply(sentence) 
-  
+  def create_reply(sentence) 
+   return pattern_match(sentence) 
   end
 end
 
+chatter = Chatter.new
+word = gets
+word = word.to_s
+puts chatter.create_reply(word)
