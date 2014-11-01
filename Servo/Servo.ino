da689fc65ede077d68a2bff039cb1da7502277d8
sh4869 <nobuk4869@gmail.com>
2014-11-01 15:26:10 +0900

    rewrite Servo.ino - Setup for Serial Communication

--- Servo/Servo.ino
+++ Servo/Servo.ino
@@ -3,25 +3,30 @@ int i,val;
 Servo myservo;
 void setup(){
   myservo.attach(9);
+  Serial.begin(9600);
 }
 
- 
+
 void loop(){
-  while(i < 45){
-    delay(20);
-	val = 45 - i;
-    myservo.write(val);
-	i++;
+  if(Serial.available()){
+	if( Serial.read() == 'T'){      
+	  Serial.println("Get!!");
+	  for(int cou = 0;cou < 5;cou++){
+		while(i < 45){
+		  delay(10);
+		  val = 45 - i;
+		  myservo.write(val);
+		  i++;
+		}
+		i = 0;
+		while(i < 45){
+		  delay(10);
+		  val = i;
+		  myservo.write(val);
+		  i++;
+		}  
+		i = 0;
+	  }
+	}
   }
-  
-  i = 0;
- 
-  while(i < 45){
-	delay(20);
-	val = i;
-	myservo.write(val);
-	i++;
-  }  
-  i = 0;
-  
 }
