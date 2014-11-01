f2ac43e61ce0a1afa055ac6c21d24c2eab030cc5
sh4869 <nobuk4869@gmail.com>
2014-11-01 18:11:43 +0900

    indented

--- chatter_bot.rb
+++ chatter_bot.rb
@@ -7,41 +7,41 @@ require './keys.rb'
 
 class Chatter
   def initilaize
-    $context = ""
+	$context = ""
   end
-  
+
   def pattern_match(sentence)
-    CSV.foreach("pattern.csv") do |pattern|
-      if sentence.include?(pattern[0])
-	    return pattern[1]
+	CSV.foreach("pattern.csv") do |pattern|
+	  if sentence.include?(pattern[0])
+		return pattern[1]
 	  end
 	end	  
 	return -1
   end
-  
+
   def docomo_reply(sentence)
-    uri = URI.parse("https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?APIKEY=#{APIKEY}")
-    http = Net::HTTP.new(uri.host, uri.port)
+	uri = URI.parse("https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?APIKEY=#{APIKEY}")
+	http = Net::HTTP.new(uri.host, uri.port)
 	http.use_ssl = true
 	body = {'utt' => sentence}
-    request = Net::HTTP::Post.new(uri.request_uri, {'Content-Type' =>'application/json'})
+	request = Net::HTTP::Post.new(uri.request_uri, {'Content-Type' =>'application/json'})
 	request.body = body.to_json
 	http.start do |h|
-	  	resp = h.request(request)
-	    response = JSON.parse(resp.body)
-		$context = response['context']
-		if response['utt'].start_with?("<head>") == false
-		  return response['utt']
-		end
+	  resp = h.request(request)
+	  response = JSON.parse(resp.body)
+	  $context = response['context']
+	  if response['utt'].start_with?("<head>") == false
+		return response['utt']
+	  end
 	end
   end
 
   def create_reply(sentence) 
-   if pattern_match(sentence) != -1
-	 return pattern_match(sentence)
-   elsif sentence.length  > 3
-	 return docomo_reply(sentence)
-   end
+	if pattern_match(sentence) != -1
+	  return pattern_match(sentence)
+	elsif sentence.length  > 3
+	  return docomo_reply(sentence)
+	end
   end
 end
 
